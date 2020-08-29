37cf4eede001faff181dcd6f593b5b0fa2aaf5f3
a3330 <a33303@mail.ru>
2020-08-30 01:04:26 +0300

    Задание 2 добавил столбцы фото и медиафайлов

--- hw3/likes.sql
+++ hw3/likes.sql
@@ -2,7 +2,9 @@ drop table if not exists 'likes';
 create table 'likes'(
 	'user_id' int(10) unsignes not null,
 	'post_id' int(10) unsignes not null,
-	key 'post_user_id' ('user_id','post_id'),
+    'comments_id' int(10) unsignes not null,
+    'photos_id' int(10) unsignes not null,
+	key 'post_user_id' ('user_id','post_id', 'comments_id', 'photos_id'),
 	key 'user_id' ('user_id')
 ) ENGINE=InnoDB;
  
